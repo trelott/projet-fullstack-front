ab2a9081b2b6793cab0f02a3e3a51d618d8c7da5
Charles Watrelot <cwatrelot@misyl.net>
2023-11-10 11:54:40 +0100

    Ajout des formulaire pour l'ajout et la modification des membres

--- src/app/back-office/menu-centres/manage-center-member/create-center-member/create-center-member.component.ts
+++ src/app/back-office/menu-centres/manage-center-member/create-center-member/create-center-member.component.ts
@@ -1,13 +1,27 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {User} from "../../../../interfaces/user";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-create-center-member',
   templateUrl: './create-center-member.component.html',
   styleUrls: ['./create-center-member.component.scss']
 })
-export class CreateCenterMemberComponent {
+export class CreateCenterMemberComponent implements OnInit{
   @Output() createDone: EventEmitter<boolean> = new EventEmitter<boolean>();
+  createCenterMemberForm!: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.createCenterMemberForm = new FormGroup({
+      email: new FormControl(''),
+      firstname: new FormControl(''),
+      lastname: new FormControl(''),
+      role: new FormControl(''),
+      center: new FormControl(''),
+    });
+  }
 
   emitCreateDone(value: boolean): void {
     this.createDone.emit(value);
@@ -16,5 +30,4 @@ export class CreateCenterMemberComponent {
   createCenterMember() {
     this.emitCreateDone(true);
   }
-
 }
--- src/app/back-office/menu-centres/manage-center-member/edit-center-member/edit-center-member.component.ts
+++ src/app/back-office/menu-centres/manage-center-member/edit-center-member/edit-center-member.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {User} from "../../../../interfaces/user";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-center-member',
@@ -9,13 +10,23 @@ import {User} from "../../../../interfaces/user";
 export class EditCenterMemberComponent implements OnInit {
   @Input() member!: User;
   @Output() editMemberDone: EventEmitter<boolean> = new EventEmitter<boolean>();
-  constructor() {
-  }
+  editCenterMemberForm!: FormGroup;
+  constructor() {}
   ngOnInit(): void {
+    this.editCenterMemberForm = new FormGroup({
+      email: new FormControl(this.member.email),
+      firstname: new FormControl(this.member.firstname),
+      lastname: new FormControl(this.member.lastname),
+      role: new FormControl(this.member.role),
+      center: new FormControl(this.member.center.name),
+    });
   }
 
   emitEditMemberDone(value: boolean) {
     this.editMemberDone.emit(value);
   }
 
+  modifyCenterMember() {
+    this.emitEditMemberDone(true);
+  }
 }
